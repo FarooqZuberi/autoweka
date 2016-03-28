17b88c036c99b06716902ccc2c6af1e1aec07ebd
FarooqZuberi <farooqahmedzuberi@gmail.com>
2016-03-28 16:47:06 +0200

    added XmlSerializable tester file

---
+++ test/java/autoweka/XmlSerializableTester.java
@@ -0,0 +1,19 @@
+package autoweka;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class XmlSerializableTester 
+{
+    
+    @Test
+    public void populateObjectFromCMDParamsTester(){
+        
+        
+    }
+
+    public static void main(String[] args){
+        XmlSerializable t = new XmlSerializable();
+        t.populateObjectFromCMDParamsTester();
+    }
+}
